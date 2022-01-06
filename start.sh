60171898842ea13482417efd324e602e67e4d198
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-07 00:21:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,1 @@
-echo "Cloning Repo...."
-git clone https://github.com/Azanpopz/Song-Downloader.git /CaptionBot
-cd /CaptionBot
-pip3 install -r requirements.txt
-echo "Starting Bot...."
 python3 main.py
