64f84bb44850c26f9ea560f852e1cccb2cbed75d
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-06 22:51:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,15 +20,17 @@ from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid
 
 
 
-import os
-import logging
-import pyrogram
-from config import Config  
-
-
 if __name__ == "__main__" :
     plugins = dict(
         root="mt_privateautocaption"
+    )
+    MoTech = pyrogram.Client(
+        "CaptionBot",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        plugins=plugins,
+        workers=300
     )
     
 Bot = Client(
