7cd1fd2da0a23248b7ac84b595dbfc703717d10a
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-06 23:07:12 +0530

    Update auto_caption.py

--- mt_privateautocaption/auto_caption.py
+++ mt_privateautocaption/auto_caption.py
@@ -8,13 +8,6 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import FloodWait
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", 12345))
-API_HASH = os.environ.get("API_HASH", "")
-CAPTION = os.environ.get("CAPTION", "@Mo_Tech_YT @Mo_Tech_Group")
-BUTTON_TEXT = os.environ.get("BUTTON", "🔻Join Channel🔻")
-URL_LINK = os.environ.get("LINK", "T.ME/MO_TECH_YT")
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
 
 
 CAPTION_TEXT=Config.CAPTION
